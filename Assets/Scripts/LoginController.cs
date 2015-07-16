320fb2843ed0b390aacc5f3dd605f9950838062b
JiangShu <jiangsyk@163.com>
2015-07-16 19:27:51 +0800

    init

---
+++ Assets/Scripts/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+/*
+ * Description: LoginController
+ * Author:      JiangShu
+ * Create Time: 2015/7/16 17:16:27
+ */
+public class LoginController : MonoBehaviour
+{
+    public InputField usernameText;
+    public InputField passwordText;
+    public Text errorTips;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+    public void OnClickLoginBtn()
+    {
+        string username = usernameText.text;
+        string password = passwordText.text;
+        if(username != "jiangshu" || password != "jiangshu")
+        {
+            errorTips.text = "帐号或密码错误！";
+            StartCoroutine(HideErrorTips());
+            return;
+        }
+        Application.LoadLevel("Menu");
+    }
+    public IEnumerator HideErrorTips()
+    {
+        yield return new WaitForSeconds(1);
+        errorTips.text = "";
+    }
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+/*
+ * Description: MenuController
+ * Author:      JiangShu
+ * Create Time: 2015/7/16 17:43:28
+ */
+public class MenuController : MonoBehaviour
+{
+    public enum GameModel
+    {
+        EASY,
+        NORMAL,
+        HARD
+    }
+    [HideInInspector]
+    public bool soundIsOn = false;
+    [HideInInspector]
+    public float soundVal = 0;
+    [HideInInspector]
+    public GameModel model = GameModel.EASY;
+
+    public Toggle soundToggle;
+    public Slider soundSlider;
+
+    public GameObject optionsView;
+    public bool optionsShow = false;
+   
+    void Start()
+    {
+        soundIsOn = soundToggle.isOn;
+        soundVal = soundSlider.value;
+
+        optionsView.transform.localPosition = new Vector3(-270, 0, 0);
+    }
+    void Update()
+    {
+
+    }
+    public void OnSoundToggleValueChanged(bool isOn)
+    {
+        soundIsOn = isOn;
+    }
+    public void OnSoundValChanged(float val)
+    {
+        soundVal = val;
+    }
+    public void OnModelChanged(string model)
+    {
+        switch(model)
+        {
+            case "easy":
+                this.model = GameModel.EASY;
+                break;
+            case "normal":
+                this.model = GameModel.NORMAL;
+                break;
+            case "hard":
+                this.model = GameModel.HARD;
+                break;
+        }
+    }
+    public void OnClickStartBtn()
+    {
+        print("SoundIsOn:" + soundIsOn + "\nSoundVal:" + soundVal + "\nGameModel:" + model);
+    }
+    public void OnClickOptionsBtn()
+    {
+        if(!optionsShow)
+        {
+            iTween.MoveTo(optionsView, iTween.Hash("position", Vector3.zero, "time", 0.5f, "islocal", true,"easetype",iTween.EaseType.linear));
+            optionsShow = true;
+        }
+        else
+        {
+            iTween.MoveTo(optionsView,iTween.Hash("position",new Vector3(-270, 0, 0),"time",0.5f,"islocal",true,"easetype",iTween.EaseType.linear));
+            optionsShow = false ;
+        }
+    }
+    public void OnClickExit()
+    {
+        Application.LoadLevel("Login");
+    }
+}
